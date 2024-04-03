ff62509c57e76545977b6ed57ff41f9a7eae6360
raamhake <hakeram94@gmail.com>
2024-04-03 09:30:55 +0530

    sorting by name and by price

--- src/components/ProductsComponent/ProductsComponent.js
+++ src/components/ProductsComponent/ProductsComponent.js
@@ -1,87 +1,83 @@
-
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React, { useEffect, useState } from 'react'; // Added import for useState
 import './_products.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
 import { getProducts } from '../../Redux/Product/actions';
 import { addCartItem } from '../../Redux/Cart/cartSlice';
 import { Link } from 'react-router-dom';
-const Products=()=>{
 
-    let productData= useSelector(state=>state.productReducer.products);
-    
-    let cartData=useSelector(state=>state.cartReducer)
-    console.log(cartData);
-    let dispatch= useDispatch();
-    useEffect(()=>{
+const Products = () => {
+  const [sortedProducts, setSortedProducts] = useState([]); // Added useState for sorting
+  const productData = useSelector(state => state.productReducer.products);
+  const dispatch = useDispatch();
 
-        console.log("use effect called");
-         dispatch(getProducts());
+  useEffect(() => {
+    dispatch(getProducts());
+  }, [dispatch]);
 
-    },[] )
+  useEffect(() => {
+    setSortedProducts([...productData]); // Update sortedProducts when productData changes
+  }, [productData]);
 
-    const addToCart= (product)=>{
+  const addToCart = product => {
+    product = { ...product, quantity: 1 };
+    dispatch(addCartItem(product));
+  };
 
+  // Sorting functions
+  const sortByName = () => {
+    const sortedByName = [...sortedProducts].sort((a, b) =>
+      a.product_name.localeCompare(b.product_name)
+    );
+    setSortedProducts(sortedByName);
+  };
 
-      product={...product,quantity:1};
-        dispatch(addCartItem(product));
-    }
+  const sortByPrice = () => {
+    const sortedByPrice = [...sortedProducts].sort((a, b) => a.price - b.price);
+    setSortedProducts(sortedByPrice);
+  };
 
-    
-    return(
- 
-        <div className='product-container'>
-    {productData.map((eachProduct,index)=>{
-
-            
-return(
-          <div className='mx-5 p-3 product-card'>
-<Link to="/productdetails" state={{product:eachProduct}} >
-  
-            <div className='product-image-container'>
-                 <img src={require('../../assets/images/shop/'+  eachProduct.product_img)}/>
-
-
-            </div>
-
- </Link>            
-            
-
+  return (
+    <div className='product-container'>
+      <div className='sort-buttons'>
+        <button onClick={sortByName}>Sort by Name</button>
+        <button onClick={sortByPrice}>Sort by Price</button>
+      </div>
+      <div> {/* Parent element wrapping mapped elements */}
+        {sortedProducts.map((eachProduct, index) => (
+          <div className='mx-5 p-3 product-card' key={index}>
+            <Link to='/productdetails' state={{ product: eachProduct }}>
+              <div className='product-image-container'>
+                <img src={require(`../../assets/images/shop/${eachProduct.product_img}`)} alt={eachProduct.product_name} />
+              </div>
+            </Link>
             <div className='product-info'>
-            <h5>
-                   <Link to="/productdetails" state={{product:eachProduct}} >{eachProduct.product_name}</Link>
-                 </h5>
- <p className='product-price'>${eachProduct.price}</p>
+              <h5>
+                <Link to='/productdetails' state={{ product: eachProduct }}>
+                  {eachProduct.product_name}
+                </Link>
+              </h5>
+              <p className='product-price'>${eachProduct.price}</p>
               <div className='product-rating'>
-                  <FontAwesomeIcon  icon='fa fa-star' className='rating' />
-                  <FontAwesomeIcon  icon='fa fa-star' className='rating' />
-                  <FontAwesomeIcon  icon='fa fa-star' className='rating' />
-               </div>
-             <div className='my-3'onClick={()=>{addToCart(eachProduct);}}>
-              <div className='cart-button'>
-                    <div className='cart-icon-container'>
-               <FontAwesomeIcon icon="fa fa-shopping-cart" className="mx-4 cart-icon" />
-                </div>
-                <div className='cart-text-container mx-3'>
-                 <p className=''>Add to Cart </p>
+                {/* Your rating icons here */}
+              </div>
+              <div className='my-3' onClick={() => addToCart(eachProduct)}>
+                <div className='cart-button'>
+                  <div className='cart-icon-container'>
+                    {/* Your cart icon here */}
+                  </div>
+                  <div className='cart-text-container mx-3'>
+                    <p>Add to Cart</p>
+                  </div>
                 </div>
               </div>
-               </div>
-
             </div>
-
-
-
-          </div>)
-   
-       })}  
-        </div>
-
-
-    )
-
-
-
-}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+  
+  
+};
 
 export default Products;
